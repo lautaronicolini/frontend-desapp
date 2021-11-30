0e4a3bfa47ba11fa6585bac90c75692f3ecc90ff
am-camila <marchinicamila94@gmail.com>
2021-11-30 01:30:27 -0300

    added loading spinner

---
+++ src/components/LoadSpinner.jsx
@@ -0,0 +1,12 @@
+import '../styles/LoadSpinner.css'
+import React from 'react';
+
+
+
+const LoadSpinner = () => {
+    return (
+        <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>)
+
+    }
+
+    export default LoadSpinner;
